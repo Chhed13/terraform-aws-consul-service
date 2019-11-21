08c3b1aad2ffb05cc4f88b02e8226163ebfae85c
chhed13 <chhed13@gmail.com>
2019-11-21 16:47:51 +0300

    Fix ACL settings

--- set_acls.sh
+++ set_acls.sh
@@ -11,8 +11,8 @@ export CONSUL_HTTP_TOKEN=${master}
 
 echo "Creating policies"
 consul acl policy create -name anonymous -rules 'agent_prefix "" { policy = "read" } event_prefix "" { policy = "read" } key_prefix "" { policy = "read" } keyring = "read" node_prefix "" { policy = "read" } operator = "read" query_prefix "" { policy = "read" } service_prefix "" { policy = "read" }'
-consul acl policy create -name agent -rules 'agent_prefix "" { policy = "write" } event_prefix "" { policy = "read" } key_prefix "" { policy = "read" } key_prefix "HotSwapServices/" { policy = "write" } keyring = "write" node_prefix "" { policy = "write" } operator = "read" query_prefix "" { policy = "read" } service_prefix "" { policy = "write" }'
-consul acl policy create -name admin -rules 'agent_prefix "" { policy = "write" } event_prefix "" { policy = "write" } key_prefix "" { policy = "write" } key_prefix "HotSwapServices/" { policy = "write" } keyring = "write" node_prefix "" { policy = "write" } operator = "write" query_prefix "" { policy = "write" } service_prefix "" { policy = "write" }'
+consul acl policy create -name agent -rules 'agent_prefix "" { policy = "write" } event_prefix "" { policy = "read" } key_prefix "" { policy = "write" } keyring = "write" node_prefix "" { policy = "write" } operator = "read" query_prefix "" { policy = "read" } service_prefix "" { policy = "write" }'
+consul acl policy create -name admin -rules 'agent_prefix "" { policy = "write" } event_prefix "" { policy = "write" } key_prefix "" { policy = "write" } keyring = "write" node_prefix "" { policy = "write" } operator = "write" query_prefix "" { policy = "write" } service_prefix "" { policy = "write" }'
 
 echo "Creating tokens"
 consul acl token create -policy-name=agent -description="agent" -secret=${agent}
